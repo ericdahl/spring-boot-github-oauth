79392a1163c95a3ec5f3ca02ae754e7d3b5bce1d
Eric Dahl <eric.c.dahl@gmail.com>
2016-01-19 19:00:52 -0800

    log message

--- src/main/java/com/github/ericdahl/spring_boot_github_oauth/oauth/AccessTokenService.java
+++ src/main/java/com/github/ericdahl/spring_boot_github_oauth/oauth/AccessTokenService.java
@@ -1,6 +1,8 @@
 package com.github.ericdahl.spring_boot_github_oauth.oauth;
 
 import com.github.ericdahl.spring_boot_github_oauth.api.ApiException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -15,6 +17,8 @@ import java.util.Map;
 
 @Service
 public class AccessTokenService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessTokenService.class);
 
     private final ParameterizedTypeReference<Map<String, String>> TYPE_REF_MAP_STRING_STRING = new ParameterizedTypeReference<Map<String, String>>() { };
 
@@ -36,6 +40,10 @@ public class AccessTokenService {
             throw new ApiException(responseEntity);
         }
 
-        return responseEntity.getBody().get("access_token");
+        final Map<String, String> responseBody = responseEntity.getBody();
+
+        LOGGER.info("access_token response payload {}", responseBody);
+
+        return responseBody.get("access_token");
     }
 }
